181e8439cf478e65c3a558c56db1f5974fc59c19
Naveen145-ai <naveensrinivas145@gmail.com>
2025-08-08 08:59:57 +0530

    seven

--- routes/potRoute.js
+++ routes/potRoute.js
@@ -4,5 +4,8 @@ const router = expresscon.Router();
 const potController = require('../controller/potController');
 
 router.post('/potAdmin', potController.potAdmin);
+router.get('/getAllPots', potController.getAllPots);
+router.put('/updatePot/:id', potController.updatePot);
+router.delete('/deletePot/:id', potController.deletePot);
 
 module.exports = router;
